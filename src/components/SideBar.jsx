de7e8e90cf0a999a7da314f8b0ab993081d5cf53
DrGooseman <jamesjquinn12@gmail.com>
2020-03-21 23:37:59 +0300

    adds content to side bar

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { NavLink } from "react-router-dom";
+
+function SideBar() {
+  return (
+    <header className="App-header">
+      <img src={"/images/profpic.jpg"} alt="profile pic" />
+      <h3>James Quinn</h3>
+      <p>
+        <i class="far fa-envelope"></i> JamesJQuinn12@gmail.com
+      </p>
+
+      <h4>American programmer living in Moscow.</h4>
+      <h4>Aspiring full-stack developer.</h4>
+      <h4>Experienced in game dev / virtual reality.</h4>
+      <h4></h4>
+
+      <h4></h4>
+
+      <Navbar bg="dark" variant="dark">
+        <Nav className="mr-auto flex-column">
+          <Nav.Link as={NavLink} to={"/"}>
+            About Me
+          </Nav.Link>
+          <Nav.Link as={NavLink} to={"/games"}>
+            Web Apps
+          </Nav.Link>
+          <Nav.Link as={NavLink} to={"/games"}>
+            Games
+          </Nav.Link>
+          <Nav.Link as={NavLink} to={"/games"}>
+            Work Experience
+          </Nav.Link>
+        </Nav>
+      </Navbar>
+      <h4></h4>
+
+      <h4></h4>
+      {/* <NavLink to={`/`}>Dashboard</NavLink>
+      <NavLink to={`/poop`}>Dashboard</NavLink> */}
+      <i className="fab fa-github"></i>
+      <i className="fab fa-linkedin"></i>
+      <i className="fab fa-xing-square"></i>
+      <i class="fab fa-hackerrank"></i>
+      {/* <img className="icon" src="/images/hh.ico" alt="hh" /> */}
+      <h4></h4>
+
+      <h4></h4>
+      <h4>Want to see this smiling face working at your company?</h4>
+      <h4>Want to get together and program?</h4>
+      <h4>Want to just meet for a pint?</h4>
+      <h4>I'm waiting to hear from you!</h4>
+    </header>
+  );
+}
+
+export default SideBar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="main-body">
+      <h1>My name is James Quinn.</h1>
+    </div>
+  );
+}
+
+export default Home;
