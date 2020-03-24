32ea85cdc01c25697ece29803db4833b6a67a4c3
DrGooseman <jamesjquinn12@gmail.com>
2020-03-24 23:06:16 +0300

    finishes my games section

--- src/pages/Games.jsx
+++ src/pages/Games.jsx
@@ -27,14 +27,14 @@ function Games() {
             <Card.Text>
               <ul>
                 <li>Quick release.</li>
-                <li>New experimental VR features</li>
-                <li>My first Unity & multiplayer game</li>
+                <li>New experimental VR features.</li>
+                <li>My first Unity & multiplayer game.</li>
               </ul>
             </Card.Text>
             <Button variant="primary">Steam Page</Button>
           </Card.Body>
         </Card>
-        <div>
+        <div className="game-info-text">
           <p>
             My first real PC game. I started working on Battle Dome very shortly
             after the first VR headsets were released. Developed very quickly in
@@ -53,46 +53,48 @@ function Games() {
       </div>
       <Jumbotron className="jumbo2">
         <h1>
-          <span>Battle Dome Redux</span>
+          <span>Inhumanus</span>
         </h1>
         <p>
           <span>
-            Battle Dome is back, and better than ever! Battle Dome Redux focuses
-            on the original game play elements of Battle Dome, refining and
-            improving upon every aspect to give you the best Battle Dome
-            experience. Battle Dome Redux is a 5 v 5 competitive Sci-Fi VR
-            shooter.
+            Inhumanus is a story-based Sci-Fi singleplayer / co-op (up to 4
+            players) virtual reality shooter. Use a variety of guns to survive a
+            complex full of aliens, robots, and mutants.
           </span>
         </p>
       </Jumbotron>
       <div className="game-info">
         <Card>
-          <Card.Img variant="top" src="/images/games/bdr2.png" />
+          <Card.Img variant="top" src="/images/games/in.png" />
           <Card.Body>
             <Card.Title>Notable Info</Card.Title>
             <Card.Text>
               <ul>
-                <li>Focus on polish/quality</li>
-                <li>Unified and deliberate theme/aesthetic</li>
+                <li>Full length campaign.</li>
+                <li>Crafted experience and scripted events.</li>
+                <li>4 player Co-op gameplay.</li>
               </ul>
             </Card.Text>
             <Button variant="primary">Steam Page</Button>
           </Card.Body>
         </Card>
-        <div>
+        <div className="game-info-text">
           <p>
-            My first real PC game. I started working on Battle Dome very shortly
-            after the first VR headsets were released. Developed very quickly in
-            order to capitalize on the virtual reality market's need for a PvP
-            shooter, I released Battle Dome about a month and a half later.
+            My first attempt at a full length campaign. Inhumanus was a lot of
+            work and took a lot of time, and I learned a lot in the process. At
+            the time of release, Inhumanus was one of the VR games with the
+            longest play times.
           </p>
           <p>
-            Battle Dome was an instant success. Afterwards, I decided to leave
-            my IT job at Northstar Travel Group and focus on developing Battle
-            Dome. I spent the next year improving the game and adding new
-            features and game modes. My focus was to push the boundaries of VR
-            and create new features that were not yet available in other VR
-            games.
+            A lot of work was also put into bug testing and ensuring a seamless
+            co-op experience. During development, I created a mouse/keyboard
+            mode just so that I could test co-op by myself, using the
+            mouse/keyboard and my VR headset simultaneously.
+          </p>
+          <p>
+            Keeping up performance was also a major challenge since the game
+            consisted of much larger levels than any of my others games, as well
+            as nicer graphics.
           </p>
         </div>
       </div>
@@ -117,27 +119,30 @@ function Games() {
             <Card.Title>Notable Info</Card.Title>
             <Card.Text>
               <ul>
-                <li>Focus on polish/quality</li>
-                <li>Unified and deliberate theme/aesthetic</li>
+                <li>Focus on polish/quality.</li>
+                <li>Unified and deliberate theme/aesthetic.</li>
+                <li>
+                  Focus on what customers wanted, and delivers the best possible
+                  version.
+                </li>
               </ul>
             </Card.Text>
             <Button variant="primary">Steam Page</Button>
           </Card.Body>
         </Card>
-        <div>
+        <div className="game-info-text">
           <p>
-            My first real PC game. I started working on Battle Dome very shortly
-            after the first VR headsets were released. Developed very quickly in
-            order to capitalize on the virtual reality market's need for a PvP
-            shooter, I released Battle Dome about a month and a half later.
+            Many fans of the original Battle Dome kept asking me for a remake, a
+            version that focused on the original idea of the first game. Battle
+            Dome Redux was my response.
           </p>
           <p>
-            Battle Dome was an instant success. Afterwards, I decided to leave
-            my IT job at Northstar Travel Group and focus on developing Battle
-            Dome. I spent the next year improving the game and adding new
-            features and game modes. My focus was to push the boundaries of VR
-            and create new features that were not yet available in other VR
-            games.
+            Rather than focus on a quick release, I spend much more time
+            deliberately crafting a higher quality and more focused experience.
+            No short cuts, no cutting corners. All graphical assets, effects,
+            and levels had to have a cohesive aesthetic. Because of this, I had
+            to create and edit assets myself at times in order to have
+            everything that I needed.
           </p>
         </div>
       </div>
